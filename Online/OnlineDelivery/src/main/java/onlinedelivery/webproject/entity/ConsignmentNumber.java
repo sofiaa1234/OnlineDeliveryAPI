f536efa5119a9b143b7b6c61a5d681d6a42f29ca
sofiaa1234 <123548962+sofiaa1234@users.noreply.github.com>
2023-08-23 14:52:27 +0530

    Update ConsignmentNumber.java

--- Online/OnlineDelivery/src/main/java/onlinedelivery/webproject/entity/ConsignmentNumber.java
+++ Online/OnlineDelivery/src/main/java/onlinedelivery/webproject/entity/ConsignmentNumber.java
@@ -1,13 +1,18 @@
 package onlinedelivery.webproject.entity;
 
 import java.util.Random;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConsignmentNumber {
     public String generateConsignmentNumber() {
-        // Generate a random consignment number logic
-        return "CONSIGNMENT NUMBER: "+new Random().nextInt(10000);
+//        // Generate a random consignment number logic
+//        return "CONSIGNMENT NUMBER: "+new Random().nextInt(10000);
+//    }
+        UUID uuid = UUID.randomUUID();
+        return "CONSIGNMENT NUMBER:" + uuid;
+
     }
 }
